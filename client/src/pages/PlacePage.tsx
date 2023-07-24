078d153384916c5a89643f27342953123ab51624
AndrikSokol <AndrikSokol31@gmail.com>
2023-07-24 14:16:09 +0300

    fix: styles for photo

--- client/src/pages/PlacePage.tsx
+++ client/src/pages/PlacePage.tsx
@@ -117,8 +117,20 @@ const PlacePage = () => {
             </div>
           </div>
           <div>
-            <div className="grid grid-cols-[75%_25%]  justify-center gap-2">
-              <div className="row-start-1 row-end-3 rounded-l-lg overflow-hidden">
+            <div
+              className={
+                place.photos?.length < 2
+                  ? "justify-center"
+                  : "grid grid-cols-[75%_25%]  justify-center gap-2"
+              }
+            >
+              <div
+                className={
+                  place.photos?.length == 1
+                    ? "row-start-1 row-end-3 rounded-lg overflow-hidden"
+                    : "row-start-1 row-end-3 rounded-l-lg overflow-hidden"
+                }
+              >
                 {place.photos?.[0] && (
                   <img
                     className="object-cover w-full h-full "
@@ -144,26 +156,28 @@ const PlacePage = () => {
                     alt=""
                   />
                 )}
-                <button
-                  onClick={() => setShowAllPhotos(true)}
-                  className="flex  gap-1 absolute bottom-2 right-2 py-2 px-2 bg-white rounded-2xl  shadow-md hover:scale-[102%] ease-in-out "
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-6 h-6"
+                {place.photos?.length > 3 && (
+                  <button
+                    onClick={() => setShowAllPhotos(true)}
+                    className="flex  gap-1 absolute bottom-2 right-2 py-2 px-2 bg-white rounded-2xl  shadow-md hover:scale-[102%] ease-in-out "
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                    />
-                  </svg>
-                  Показать фото
-                </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                      />
+                    </svg>
+                    Показать фото
+                  </button>
+                )}
               </div>
             </div>
           </div>
