06bc9942b73bb4e99bc3483c6e746468f9db1462
AndrikSokol <AndrikSokol31@gmail.com>
2023-09-12 03:12:05 +0300

    fix: fixed bugs

--- api/config/passport-config.js
+++ api/config/passport-config.js
@@ -3,7 +3,6 @@ const passport = require("passport");
 const GoogleUser = require("../models/GoogleUser");
 const TokenService = require("../services/token-service");
 require("dotenv").config();
-const speakeasy = require("speakeasy");
 
 passport.use(
   new GoogleStrategy(
@@ -17,13 +16,13 @@ passport.use(
       let googleUser = await GoogleUser.findOne({ googleId: userJson.sub });
       const tokens = TokenService.generateTokens({ accessToken });
       TokenService.saveToken(userJson.googleId, tokens.refreshToken);
-      const secret = speakeasy.generateSecret({ length: 10 });
+
       if (googleUser) return done(null, { googleUser, ...tokens });
       googleUser = new GoogleUser({
         googleId: userJson.sub,
         email: userJson.email,
         username: userJson.name,
-        secretKey: secret,
+        // secretKey: secret,
       });
       await googleUser.save();
       delete googleUser.secretKey;
